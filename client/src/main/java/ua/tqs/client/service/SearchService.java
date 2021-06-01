e73c1f08453dfad6b020b31400d26d6d8de557f4
Duarte Mortágua <duarte.ntm@ua.pt>
2021-06-01 20:51:11 +0100

    Created search and orderPlacement services. Started tests for search service.

---
+++ client/src/main/java/ua/tqs/client/service/SearchService.java
@@ -0,0 +1,11 @@
+package ua.tqs.client.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+    public JsonNode getProductsBySearchParams(String searchQuery, String searchOrderBy, String searchFilter, String apiKey) {
+       return null;
+    }
+}
