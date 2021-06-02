c6f15190c34bfca9893e3b7c3a9c2577f52f7ce3
Duarte Mortágua <duarte.ntm@ua.pt>
2021-06-02 19:28:35 +0100

    Changed appid to App entity.

--- cito/src/main/java/ua/tqs/cito/service/ProductService.java
+++ cito/src/main/java/ua/tqs/cito/service/ProductService.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ua.tqs.cito.Product;
-import ua.tqs.cito.ProductRepository;
+import ua.tqs.cito.model.Product;
+import ua.tqs.cito.repository.ProductRepository;
 
 
 @Service
@@ -15,7 +15,7 @@ public class ProductService {
 	private ProductRepository productRepository;
 	
 	public Product save(Product p) {
-		if (p.getName() == null || p.getAppId() == null || p.getDescription() == null || p.getImage() == null || p.getPrice() == null || p.getCategory() == null) {
+		if (p.getName() == null || p.getApp() == null || p.getDescription() == null || p.getImage() == null || p.getPrice() == null || p.getCategory() == null) {
 			return null;
 		}
 		return productRepository.save(p);
