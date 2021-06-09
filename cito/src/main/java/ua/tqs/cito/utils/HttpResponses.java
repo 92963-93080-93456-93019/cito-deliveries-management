cb6ef194c24b3b41acf58c373f358f2b75a4625a
Tiago Oliveira <tiago.srv.oliveira@ua.pt>
2021-06-09 21:14:38 +0100

    Testes do order service finalizados

--- cito/src/main/java/ua/tqs/cito/utils/HttpResponses.java
+++ cito/src/main/java/ua/tqs/cito/utils/HttpResponses.java
@@ -9,7 +9,6 @@ public class HttpResponses {
     public static final String INVALID_ADDRESS = "{\"code\" : 403, \"message\" : \"No delivery address provided.\"}";
     public static final String ORDER_SAVED = "{\"code\" : 201, \"message\" : \"Order saved.\"}";
     public static final String ORDER_UPDATED = "{\"code\" : 201, \"message\" : \"Order updated (#).\"}";
-    public static final String ORDER_NOT_SAVED = "{\"code\" : 500, \"message\" : \"Bad parameters for order.\"}";
     public static final String PRODUCT_SAVED = "{\"code\" : 201, \"message\" : \"Product saved.\"}";
     public static final String PRODUCT_NOT_SAVED = "{\"code\" : 500, \"message\" : \"Bad parameters for product.\"}";
     public static final String MANAGER_NOT_FOUND_FOR_APP = "{\"code\" : 404, \"message\" : \"Manager not found for app.\"}";
